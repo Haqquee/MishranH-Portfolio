4cbceaa9a153759a4e84a3616375ddff8d6a2635
Mishran <mugdho2009@yahoo.com>
2023-03-13 13:06:53 -0400

    Updated nav component

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import './nav.css'
+import { useState } from 'react'
 import {FaHome} from 'react-icons/fa'
 import {SiAboutdotme} from 'react-icons/si'
 import {IoIosRocket} from 'react-icons/io'
 import {GrMail} from 'react-icons/gr'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#"><FaHome/></a>
-      <a href="#about"><SiAboutdotme/></a>
-      <a href="#projects"><IoIosRocket/></a>
-      <a href="#contact"><GrMail/></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><FaHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><SiAboutdotme/></a>
+      <a href="#projects" onClick={() => setActiveNav('#projects')} className={activeNav === '#projects' ? 'active' : ''}><IoIosRocket/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><GrMail/></a>
     </nav>
   )
 }
