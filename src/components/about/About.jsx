75613c738712623a3791f582ec8cf1700a9c1c68
Mishran <mugdho2009@yahoo.com>
2023-03-13 15:02:24 -0400

    Updated about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,15 @@ import './about.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section>
+      <h1>About Me</h1>
+      <div id='about' className="container about__container">
+        <p>Hello! </p>
+        <p>My name is Mishran Haque. </p>
+        <p>I am an Aerospace Engineering student at Ryerson University. Space and everything extraterrestrial was fascinating to me from early childhood and throughout my High School years. This interest soon evolved into the unavoidable career choice towards Science & Engineering. </p>
+        <p>My passion for space travel and state-of-the-art vehicles eventually lead me to join the Aerospace Engineering Program at Ryerson University. Over my three years of university, I got to take various courses that strengthened my knowledge of aerospace applications. I got the opportunity to work with Ryerson Rocketry Club and experience the process behind designing and building a supersonic rocket. Furthermore, I also worked on various projects throughout my years, which involved researching and designing gliders and programming robots that perform various tasks. </p>
+      </div>
+    </section>
   )
 }
 
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,7 +3,22 @@ import './projects.css'
 
 const Projects = () => {
   return (
-    <section id='projects'></section>
+    <section id='projects'>
+      <h1>Experiences and Projects</h1>
+      <div className="container container__projects">
+        <article>
+          <a href="">
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <p></p>
+          </div>
+          </a>
+        </article>
+      </div>
+    </section>
+    
   )
 }
 
